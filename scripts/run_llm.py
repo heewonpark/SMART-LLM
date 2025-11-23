6f419caaccd2de03202e212464abed580bedd100
Heewon Park <park.brainlab@gmail.com>
2025-11-23 12:45:10 -0800

    update old openai code

--- scripts/run_llm.py
+++ scripts/run_llm.py
@@ -8,7 +8,9 @@ from datetime import datetime
 import random
 import subprocess
 
-import openai
+from openai import OpenAI
+openai_api_key = "api_key"
+client = OpenAI(api_key=Path(openai_api_key + '.txt').read_text())
 import ai2thor.controller
 
 import sys
@@ -19,29 +21,29 @@ import resources.robots as robots
 
 
 def LM(prompt, gpt_version, max_tokens=128, temperature=0, stop=None, logprobs=1, frequency_penalty=0):
-    
+
     if "gpt" not in gpt_version:
-        response = openai.Completion.create(model=gpt_version, 
+        response = client.completions.create(model=gpt_version, 
                                             prompt=prompt, 
                                             max_tokens=max_tokens, 
                                             temperature=temperature, 
                                             stop=stop, 
                                             logprobs=logprobs, 
                                             frequency_penalty = frequency_penalty)
-        
-        return response, response["choices"][0]["text"].strip()
-    
+
+        return response, response.choices[0].text.strip()
+
     else:
-        response = openai.ChatCompletion.create(model=gpt_version, 
+        response = client.chat.completions.create(model=gpt_version, 
                                             messages=prompt, 
                                             max_tokens=max_tokens, 
                                             temperature=temperature, 
                                             frequency_penalty = frequency_penalty)
-        
-        return response, response["choices"][0]["message"]["content"].strip()
+
+        return response, response.choices[0].message.content.strip()
 
 def set_api_key(openai_api_key):
-    openai.api_key = Path(openai_api_key + '.txt').read_text()
+    OpenAI.api_key = Path(openai_api_key + '.txt').read_text()    
 
 # Function returns object list with name and properties.
 def convert_to_dict_objprop(objs, obj_mass):
@@ -67,25 +69,25 @@ if __name__ == "__main__":
     parser.add_argument("--openai-api-key-file", type=str, default="api_key")
     parser.add_argument("--gpt-version", type=str, default="gpt-4", 
                         choices=['gpt-3.5-turbo', 'gpt-4', 'gpt-3.5-turbo-16k'])
-    
+
     parser.add_argument("--prompt-decompse-set", type=str, default="train_task_decompose", 
                         choices=['train_task_decompose'])
-    
+
     parser.add_argument("--prompt-allocation-set", type=str, default="train_task_allocation", 
                         choices=['train_task_allocation'])
-    
+
     parser.add_argument("--test-set", type=str, default="final_test", 
                         choices=['final_test'])
-    
+
     parser.add_argument("--log-results", type=bool, default=True)
-    
+
     args = parser.parse_args()
 
     set_api_key(args.openai_api_key_file)
-    
+
     if not os.path.isdir(f"./logs/"):
         os.makedirs(f"./logs/")
-        
+
     # read the tasks        
     test_tasks = []
     robots_test_tasks = []  
@@ -99,7 +101,7 @@ if __name__ == "__main__":
             gt_test_tasks.append(list(json.loads(line).values())[2])
             trans_cnt_tasks.append(list(json.loads(line).values())[3])
             max_trans_cnt_tasks.append(list(json.loads(line).values())[4])
-                    
+
     print(f"\n----Test set tasks----\n{test_tasks}\nTotal: {len(test_tasks)} tasks\n")
     # prepare list of robots for the tasks
     available_robots = []
@@ -111,30 +113,30 @@ if __name__ == "__main__":
             rob['name'] = 'robot' + str(i+1)
             task_robots.append(rob)
         available_robots.append(task_robots)
-        
-    
+
+
     ######## Train Task Decomposition ########
-        
+
     # prepare train decompostion demonstration for ai2thor samples
     prompt = f"from skills import " + actions.ai2thor_actions
     prompt += f"\nimport time"
     prompt += f"\nimport threading"
     objects_ai = f"\n\nobjects = {get_ai2_thor_objects(args.floor_plan)}"
     prompt += objects_ai
-    
+
     # read input train prompts
     decompose_prompt_file = open(os.getcwd() + "/data/pythonic_plans/" + args.prompt_decompse_set + ".py", "r")
     decompose_prompt = decompose_prompt_file.read()
     decompose_prompt_file.close()
-    
+
     prompt += "\n\n" + decompose_prompt
-    
+
     print ("Generating Decompsed Plans...")
-    
+
     decomposed_plan = []
     for task in test_tasks:
         curr_prompt =  f"{prompt}\n\n# Task Description: {task}"
-        
+
         if "gpt" not in args.gpt_version:
             # older gpt versions
             _, text = LM(curr_prompt, args.gpt_version, max_tokens=1000, stop=["def"], frequency_penalty=0.15)
@@ -143,21 +145,21 @@ if __name__ == "__main__":
             _, text = LM(messages,args.gpt_version, max_tokens=1300, frequency_penalty=0.0)
 
         decomposed_plan.append(text)
-        
+
     print ("Generating Allocation Solution...")
 
     ######## Train Task Allocation - SOLUTION ########
     prompt = f"from skills import " + actions.ai2thor_actions
     prompt += f"\nimport time"
     prompt += f"\nimport threading"
-    
+
     prompt_file = os.getcwd() + "/data/pythonic_plans/" + args.prompt_allocation_set + "_solution.py"
     allocated_prompt_file = open(prompt_file, "r")
     allocated_prompt = allocated_prompt_file.read()
     allocated_prompt_file.close()
-    
+
     prompt += "\n\n" + allocated_prompt + "\n\n"
-    
+
     allocated_plan = []
     for i, plan in enumerate(decomposed_plan):
         no_robot  = len(available_robots[i])
@@ -172,35 +174,35 @@ if __name__ == "__main__":
         if "gpt" not in args.gpt_version:
             # older versions of GPT
             _, text = LM(curr_prompt, args.gpt_version, max_tokens=1000, stop=["def"], frequency_penalty=0.65)
-        
+
         elif "gpt-3.5" in args.gpt_version:
             # gpt 3.5 and its variants
             messages = [{"role": "user", "content": curr_prompt}]
             _, text = LM(messages, args.gpt_version, max_tokens=1500, frequency_penalty=0.35)
-        
+
         else:          
             # gpt 4.0
             messages = [{"role": "system", "content": "You are a Robot Task Allocation Expert. Determine whether the subtasks must be performed sequentially or in parallel, or a combination of both based on your reasoning. In the case of Task Allocation based on Robot Skills alone - First check if robot teams are required. Then Ensure that robot skills or robot team skills match the required skills for the subtask when allocating. Make sure that condition is met. In the case of Task Allocation based on Mass alone - First check if robot teams are required. Then Ensure that robot mass capacity or robot team combined mass capacity is greater than or equal to the mass for the object when allocating. Make sure that condition is met. In both the Task Task Allocation based on Mass alone and Task Allocation based on Skill alone, if there are multiple options for allocation, pick the best available option by reasoning to the best of your ability."},{"role": "system", "content": "You are a Robot Task Allocation Expert"},{"role": "user", "content": curr_prompt}]
             _, text = LM(messages, args.gpt_version, max_tokens=400, frequency_penalty=0.69)
 
         allocated_plan.append(text)
-    
+
     print ("Generating Allocated Code...")
-    
+
     ######## Train Task Allocation - CODE Solution ########
 
     prompt = f"from skills import " + actions.ai2thor_actions
     prompt += f"\nimport time"
     prompt += f"\nimport threading"
     prompt += objects_ai
-    
+
     code_plan = []
 
     prompt_file1 = os.getcwd() + "/data/pythonic_plans/" + args.prompt_allocation_set + "_code.py"
     code_prompt_file = open(prompt_file1, "r")
     code_prompt = code_prompt_file.read()
     code_prompt_file.close()
-    
+
     prompt += "\n\n" + code_prompt + "\n\n"
 
     for i, (plan, solution) in enumerate(zip(decomposed_plan,allocated_plan)):
@@ -209,7 +211,7 @@ if __name__ == "__main__":
         curr_prompt += f"\n\nrobots = {available_robots[i]}"
         curr_prompt += solution
         curr_prompt += f"\n# CODE Solution  \n"
-        
+
         if "gpt" not in args.gpt_version:
             # older versions of GPT
             _, text = LM(curr_prompt, args.gpt_version, max_tokens=1000, stop=["def"], frequency_penalty=0.30)
@@ -219,22 +221,22 @@ if __name__ == "__main__":
             _, text = LM(messages, args.gpt_version, max_tokens=1400, frequency_penalty=0.4)
 
         code_plan.append(text)
-    
+
     # save generated plan
     exec_folders = []
     if args.log_results:
         line = {}
         now = datetime.now() # current date and time
         date_time = now.strftime("%m-%d-%Y-%H-%M-%S")
-        
+
         for idx, task in enumerate(test_tasks):
             task_name = "{fxn}".format(fxn = '_'.join(task.split(' ')))
             task_name = task_name.replace('\n','')
             folder_name = f"{task_name}_plans_{date_time}"
             exec_folders.append(folder_name)
-            
+
             os.mkdir("./logs/"+folder_name)
-     
+
             with open(f"./logs/{folder_name}/log.txt", 'w') as f:
                 f.write(task)
                 f.write(f"\n\nGPT Version: {args.gpt_version}")
@@ -247,10 +249,9 @@ if __name__ == "__main__":
 
             with open(f"./logs/{folder_name}/decomposed_plan.py", 'w') as d:
                 d.write(decomposed_plan[idx])
-                
+
             with open(f"./logs/{folder_name}/allocated_plan.py", 'w') as a:
                 a.write(allocated_plan[idx])
-                
+
             with open(f"./logs/{folder_name}/code_plan.py", 'w') as x:
                 x.write(code_plan[idx])
-            
